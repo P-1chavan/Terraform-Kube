dbf8bd1311d63859eadc33902747625a7a8e4d45
P-1chavan <chavanpawan27@gmail.com>
2024-08-04 04:34:56 +0530

    successfully done

--- versions.tf
+++ versions.tf
@@ -1,22 +1,36 @@
-
 terraform {
-  required_version = ">= 0.12"
+
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.20.0"
-    }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.58.0"
+      version = "~> 4.15.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.1.0"
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
     }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.1"
     }
+
   }
+
+backend "remote" {
+		hostname = "app.terraform.io"
+		organization = "CloudQuickLabs"
+
+		workspaces {
+			name = "AWSEKS"
+		}
+	}
+}
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
 }
