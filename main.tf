cc95b16fc6649996ceeeb2b20b6091535871b988
P-1chavan <chavanpawan27@gmail.com>
2024-08-04 03:08:50 +0530

    successfully done

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "aws1" {
+provider "aws" {
   region = var.aws_region
 }
 
@@ -9,7 +9,7 @@ resource "random_string" "suffix1" {
 
 module "vpc1" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
+  version = "5.30.0"
 
   name                 = "Pawan-eks-vpc"
   cidr                 = var.vpc_cidr
